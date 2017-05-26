1462929e17ddb6886640c87bf0a4a644bf13d86c
qftgtr <qft.gtr@gmail.com>
2017-05-27 01:10:55 +0800

    [self->_motionManager stopDeviceMotionUpdates];

--- RNMotionManager/Attitude.m
+++ RNMotionManager/Attitude.m
@@ -19,7 +19,7 @@ RCT_EXPORT_MODULE();
 - (id) init {
     self = [super init];
     NSLog(@"Attitude");
-    
+
     if (self) {
         self->_motionManager = [[CMMotionManager alloc] init];
         //Attitude
@@ -44,7 +44,7 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(setAttitudeUpdateInterval:(double) interval) {
     NSLog(@"setAttitudeUpdateInterval: %f", interval);
-    
+
     [self->_motionManager setDeviceMotionUpdateInterval:interval];
 }
 
@@ -58,9 +58,9 @@ RCT_EXPORT_METHOD(getAttitudeData:(RCTResponseSenderBlock) cb) {
     double roll = self->_motionManager.deviceMotion.attitude.roll;
     double yaw = self->_motionManager.deviceMotion.attitude.yaw;
     double pitch = self->_motionManager.deviceMotion.attitude.pitch;
-    
+
     NSLog(@"getAttitudeData: %f, %f, %f", roll, yaw, pitch);
-    
+
     cb(@[[NSNull null], @{
              @"attitude": @{
                      @"roll" : [NSNumber numberWithDouble:roll],
@@ -74,7 +74,7 @@ RCT_EXPORT_METHOD(getAttitudeData:(RCTResponseSenderBlock) cb) {
 RCT_EXPORT_METHOD(startAttitudeUpdates) {
     NSLog(@"startAttitudeUpdates");
     [self->_motionManager startDeviceMotionUpdates];
-    
+
     /* Receive the ccelerometer data on this block */
     [self->_motionManager startDeviceMotionUpdatesToQueue:[NSOperationQueue mainQueue]
                                                withHandler:^(CMDeviceMotion *motionData, NSError *error)
@@ -83,7 +83,7 @@ RCT_EXPORT_METHOD(startAttitudeUpdates) {
          double yaw = motionData.attitude.yaw;
          double pitch = motionData.attitude.pitch;
          NSLog(@"startAttitudeUpdates: %f, %f, %f", roll, yaw, pitch);
-         
+
          [self.bridge.eventDispatcher sendDeviceEventWithName:@"AttitudeData" body:@{
                                                                                          @"attitude": @{
                                                                                                  @"roll" : [NSNumber numberWithDouble:roll],
@@ -92,12 +92,12 @@ RCT_EXPORT_METHOD(startAttitudeUpdates) {
                                                                                                  }
                                                                                          }];
      }];
-    
+
 }
 
 RCT_EXPORT_METHOD(stopAttitudeUpdates) {
     NSLog(@"stopAttitudeUpdates");
-    [self->_motionManager stopAttitudeUpdates];
+    [self->_motionManager stopDeviceMotionUpdates];
 }
 
 @end
